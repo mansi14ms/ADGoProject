00259a9573eb8acc66918af24e38add85761cae6
mansi <mansi@hcg.com>
2021-08-25 20:07:49 +0530

    Added comments

--- main_test.go
+++ main_test.go
@@ -6,16 +6,7 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-
-	got := Add(4, 6)
-	want := 10
-
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
-}
-
+//Testing Home Page
 func TestHomePage(t *testing.T) {
 
 	req, err := http.NewRequest("GET", "/homePage", nil)
